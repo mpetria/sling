f871791beaacf0755999e1dbdd893bccc9d6dbba
Marius Petria <mpetria@adobe.com>
2015-02-19 16:39:32 +0200

    Some minor tweaks

--- bundles/extensions/serviceusermapper/src/main/java/org/apache/sling/serviceusermapping/ServiceUserMapping.java
+++ bundles/extensions/serviceusermapper/src/main/java/org/apache/sling/serviceusermapping/ServiceUserMapping.java
@@ -1,12 +1,12 @@
 package org.apache.sling.serviceusermapping;
 
 /**
- * The <code>ServiceUserMapping</code> service can be used to retrieve an already register service user mapping.
+ * The <code>ServiceUserMapping</code> service can be used to retrieve an already registered service user mapping.
  * A service reference targeting a service user mapping will be satisfied only when <code>ServiceUserMapper.getServiceUserID</code>
- * will return the registered user ID.
+ * will return the registered user ID in that bundle.
  * For example setting the reference target to "(subServiceName=mySubService)"
- * ensures that your component only starts when the subService is available.
- *
+ * ensures that your component only starts when the subService is available. Trying to reference a sub service from a bundle
+ * for which it was not registered for will not work.
  */
 public interface ServiceUserMapping {
 
@@ -24,12 +24,14 @@ public interface ServiceUserMapping {
 
     /**
      * Returns the service name for this mapping.
+     *
      * @return The service name for this mapping.
      */
     String getServiceName();
 
     /**
      * Returns the sub service name for this mapping.
+     *
      * @return The sub service name for this mapping. This can be {@code null} if no sub service name is configured for this mapping.
      */
     String getSubServiceName();
